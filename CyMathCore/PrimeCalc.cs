ee4d02f768b785f67d8fdebb2168a337ecde0bfc
CyMark <CyMark@users.noreply.github.com>
2022-04-02 16:30:20 +0200

    CyMathCore RELEASE V3.2 BUILD 13 Revision 108 Date: 2 April 2022 - PrimeCalc:  - Seems like all the PrimeCalc actions are working now.

--- CyMathCore/PrimeCalc.cs
+++ CyMathCore/PrimeCalc.cs
@@ -9,11 +9,13 @@ namespace CyMathCore
     {
         public PrimeCalc()
         {
-            Primes = new() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
+            Primes = new() { 2, 3, 5 };//, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
             //[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+            FoundPrimes = new() { 1, 2, 3 };
         }
 
         public List<long> Primes { get; private set; }
+        public List<long> FoundPrimes { get; private set; }
 
         long MaxPrime { get { return Primes[^1]; } }
 
@@ -36,56 +38,45 @@ namespace CyMathCore
         public bool IsPrime(int checkVal) => IsPrime((long)checkVal);
         public bool IsPrime(long checkVal)
         {
+            //throw new Exception($"check={checkVal}");
             if(checkVal <= 1) { return false; }
 
             if (checkVal == 2 || checkVal == 3 || checkVal == 5) { return true; }
 
             if (checkVal % 2 == 0 || checkVal % 3 == 0 || checkVal % 5 == 0) { return false; }
-
-
-            if (Primes.Find(q => q == checkVal) != checkVal)
+            
+            if (FoundPrimes.Find(q => q == checkVal) != checkVal)
             {
-                // first check existing list
-                /*foreach (long val in Primes)
+
+                foreach (long val in Primes)
                 {
-                    if (checkVal % val == 0) { return false; } //checkval divisble by a smaller prime
+                    //if (checkVal == 188489971511) { throw new Exception($"IsPrim: MAX={MaxPrime},val={val}"); }
+                    if (val * val > checkVal) { FoundPrimes.Add(checkVal); return true; } // found!
+                    if (checkVal % val == 0) { return false; }
                 }
-                int startIndex = Primes.Count;
-                */
-                //int maxCheck = (int)Math.Sqrt(checkVal);
 
-                if(checkVal > MaxPrime*MaxPrime)
+                if (checkVal > MaxPrime*MaxPrime)
                 {
-
-                    //long nextValToCheck = MaxPrime + 2;
-                    for (long i = MaxPrime + 2; i * i <= checkVal; i += 6)
+                    
+                    for (long i = 5; i * i <= checkVal; i += 6)
                     {
-
+                        if (i < MaxPrime) { continue; }
+                        
+                        if (i > MaxPrime && IsPrime(i)) { Primes.Add(i); FoundPrimes.Add(i);/* throw new Exception($"i={i}");*/ };
+                        if ((i + 2) > MaxPrime &&  IsPrime(i + 2)) { Primes.Add(i + 2); FoundPrimes.Add(i + 2);/* throw new Exception($"i+2={i+2}");*/ };
                         if (checkVal % i == 0 || checkVal % (i + 2) == 0)
                         { return false; }
-                        //if (IsPrime(i)) { Primes.Add(i); };
-                        //if (IsPrime(i+2)) { Primes.Add(i+2); throw new Exception($"i={i},Mx={MaxPrime}"); };
+                        //if (checkVal == 188489971511 && MaxPrime > 83) { throw new Exception($"IsPrim: MAX={MaxPrime},i={i},NV={checkVal}"); }
                         //else { Primes.Add(i); return true; }
                     }
-                    return true;
-
-                    // add more primes
-                    //for (long p = MaxPrime + 2; p <= checkVal/2; p += 2)
-                    //{
-                    //    if(p % 5 != 0)
-                    //    {
-                    //        if (IsPrime(p)) { Primes.Add(p); }
-                    //    }
-                    //}
+                    //FoundPrimes.Add(checkVal);
+                    //if (checkVal == 188489971511) { throw new Exception($"IsPrim TRUE: MAX={MaxPrime},NV={checkVal}"); }
+                    //return true;
                 }
+                //if (checkVal == 188489971511) { throw new Exception($"IsPrim FALSE: MAX={MaxPrime},NV={checkVal}"); }
+                
 
-                foreach(long val in Primes)
-                {
-                    if(val*val > checkVal) { return true; } // found!
-                    if (checkVal % val == 0) { return false; } 
-                }
-
-                return true; // end of list
+                return true; // end of list so must be prime
             }
             return true;
 
@@ -124,17 +115,20 @@ namespace CyMathCore
         {
             List<PrimeTuple> factors = new();
 
-            if(IsPrime(nrToEvaluate)) { return factors; }
 
-            //int maxCheck = (int)Math.Sqrt(nrToEvaluate);
+            if (IsPrime(nrToEvaluate)) { return factors; }
 
+            long maxStart = MaxPrime;
             long nValue = nrToEvaluate;
 
+            // first check all those in the existing list
             foreach (long val in Primes)
             {
-                if (val*val > nrToEvaluate) { return factors; } // done!
+                if (val * val > nrToEvaluate) { return factors; } // done!
+                //factors = AddFactors(val, nValue, factors);
                 while (nValue % val == 0)
                 {
+                    //if (nrToEvaluate == 188489971511) { throw new Exception($"val={val},NV={nValue}"); }
                     PrimeTuple tup = factors.Where(q => q.Prime == val).FirstOrDefault();
                     if(tup == null)
                     {
@@ -147,17 +141,182 @@ namespace CyMathCore
                     }
                     nValue /= val;
                 }
-                if(IsPrime(nValue))
+            }
+
+            //throw new Exception($"NV={nValue},factors={factors.Count}");
+
+            // check other nrs
+            //nValue = nrToEvaluate;
+            for (long i = 5; i*i < nrToEvaluate; i += 6)
+            {
+                //if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) { continue; } // these already taken care of in first check
+
+                if (i + 2 <= MaxPrime) { continue; }
+                //if (nValue*nValue > nrToEvaluate) { return factors; }
+                //if (i >= 3) { throw new Exception($"i={i},nv={nValue}"); }
+                if (i * i > nValue) { break; }
+                if (IsPrime(i))
                 {
-                    PrimeTuple nTup = new() { Prime = nValue, Count = 1 };
-                    factors.Add(nTup);
-                    break;
+                    while (nValue % i == 0)
+                    {
+                        //if(i > MaxPrime) { throw new Exception($"i={i},nv={nValue},factors={factors.Count}"); }
+                        
+                        PrimeTuple tup = factors.Where(q => q.Prime == i).FirstOrDefault();
+                        if (tup == null)
+                        {
+                            PrimeTuple nTup = new() { Prime = i, Count = 1 };
+                            factors.Add(nTup);
+                        }
+                        else
+                        {
+                            tup.Count++;
+                        }
+                        nValue /= i;
+                    }
                 }
+
+                if (IsPrime(i+2))
+                {
+                    
+                    while (nValue % (i+2) == 0)
+                    {
+                        //if (i+2 > MaxPrime) { throw new Exception($"i+2={i + 2},nv={nValue},factors={factors.Count}");  }
+                        
+                        PrimeTuple tup = factors.Where(q => q.Prime == (i+2)).FirstOrDefault();
+                        if (tup == null)
+                        {
+                            PrimeTuple nTup = new() { Prime = i+2, Count = 1 };
+                            factors.Add(nTup);
+                        }
+                        else
+                        {
+                            tup.Count++;
+                        }
+                        nValue /= (i + 2);
+                    }
+                }
+
+            }
+            //throw new Exception($"Nval={nValue},fcount={factors.Count}");
+            if (IsPrime(nValue))
+            {
+                PrimeTuple nTup = new() { Prime = nValue, Count = 1 };
+                factors.Add(nTup);
+                //throw new Exception($"NV={nValue},fcount={factors.Count}");
             }
 
             return factors;
 
         } // GetPrimeFactors
+
+
+
+        public List<PrimeTuple> GetPrimeFactors2(long nrToEvaluate)
+        {
+            List<PrimeTuple> factors = new();
+            //throw new Exception($"Max={MaxPrime}");
+
+            if (IsPrime(nrToEvaluate)) { return factors; }
+
+            //int maxCheck = (int)Math.Sqrt(nrToEvaluate);
+            
+            long maxStart = MaxPrime;
+            long nValue = nrToEvaluate;
+
+            // first check all those in the existing list
+            foreach (long val in Primes)
+            {
+                if (val * val > nrToEvaluate) { return factors; } // done!
+                //factors = AddFactors(val, nValue, factors);
+                while (nValue % val == 0)
+                {
+                    PrimeTuple tup = factors.Where(q => q.Prime == val).FirstOrDefault();
+                    if (tup == null)
+                    {
+                        PrimeTuple nTup = new() { Prime = val, Count = 1 };
+                        factors.Add(nTup);
+                    }
+                    else
+                    {
+                        tup.Count++;
+                    }
+                    nValue /= val;
+                }
+                //if(IsPrime(nValue))
+                //{
+                //    PrimeTuple nTup = new() { Prime = nValue, Count = 1 };
+                //    factors.Add(nTup);
+                //    break;
+                //}
+            }
+
+            // check other nrs
+            //nValue = nrToEvaluate;
+            for (long i = 5; i * i < nrToEvaluate; i++)
+            {
+                if (i <= MaxPrime) { continue; }
+                if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) { continue; } 
+
+                //if (i >= 3) { throw new Exception($"i={i},nv={nValue}"); }
+
+                if (IsPrime(i))
+                {
+
+                    while (nValue % i == 0)
+                    {
+                        //if(i >  7 ) { throw new Exception($"i={i},nv={nValue}"); }
+
+                        PrimeTuple tup = factors.Where(q => q.Prime == i).FirstOrDefault();
+                        if (tup == null)
+                        {
+                            PrimeTuple nTup = new() { Prime = i, Count = 1 };
+                            factors.Add(nTup);
+                        }
+                        else
+                        {
+                            tup.Count++;
+                        }
+                        nValue /= i;
+                    }
+                }
+
+            }
+            //throw new Exception($"Nval={nValue},fcount={factors.Count}");
+            if (IsPrime(nValue))
+            {
+                PrimeTuple nTup = new() { Prime = nValue, Count = 1 };
+                factors.Add(nTup);
+                //throw new Exception($"NV={nValue},fcount={factors.Count}");
+                //break;
+            }
+
+            return factors;
+
+        } // GetPrimeFactors
+
+
+
+
+
+        //List<PrimeTuple> AddFactors(out long nValue, long nrToEvaluate, long val, List<PrimeTuple> factors)
+        //{
+        //    nValue = nrToEvaluate;
+        //    while (nValue % val == 0)
+        //    {
+        //        PrimeTuple tup = factors.Where(q => q.Prime == val).FirstOrDefault();
+        //        if (tup == null)
+        //        {
+        //            PrimeTuple nTup = new() { Prime = val, Count = 1 };
+        //            factors.Add(nTup);
+        //        }
+        //        else
+        //        {
+        //            tup.Count++;
+        //        }
+        //        nValue /= val;
+        //    }
+        //    return factors;
+        //}
 
 
         public string PrimeFactorsToView(List<PrimeTuple> factors)
--- XUnitTestCyMath/XTestPrimeCalc.cs
+++ XUnitTestCyMath/XTestPrimeCalc.cs
@@ -13,13 +13,14 @@ namespace XUnitTestCyMath
         {
             PrimeCalc calc = new();
 
-            Assert.Equal(71, calc.Primes[^1]);
-            Assert.True(calc.IsPrime(9413));
+            //Assert.Equal(71, calc.Primes[^1]);
+
             Assert.True(calc.IsPrime(83));
+            Assert.Equal(7, calc.Primes[^1]);
             Assert.True(calc.IsPrime(89));
             Assert.True(calc.IsPrime(97));
-            //Assert.Equal(97, calc.Primes[^1]);
-            //Assert.Equal("[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]", calc.PrimeListToView(25));
+            Assert.True(calc.IsPrime(9413));
+            Assert.Equal("[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]", calc.PrimeListToView(25));
             //Assert.True(calc.IsPrime(1000000403));
 
         }
@@ -35,11 +36,28 @@ namespace XUnitTestCyMath
         public void PrimeCalc_ListView(int firstNrOfPrimes, string result)
         {
             PrimeCalc calc = new();
-            _ = calc.IsPrime(97*97*2); // force creation of list above 97 * 97
+            _ = calc.IsPrime(9049); // force creation of list above 97 * 97
 
             Assert.Equal(result, calc.PrimeListToView(firstNrOfPrimes));
 
         }
+
+        [Theory]
+        [InlineData(1, "[97]")]
+        [InlineData(2, "[89,97]")]
+        [InlineData(3, "[83,89,97]")]
+        [InlineData(10, "[53,59,61,67,71,73,79,83,89,97]")]
+        [InlineData(20, "[13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]")]
+        [InlineData(25, "[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]")]
+        public void PrimeCalc_ListViewDesc(int firstNrOfPrimes, string result)
+        {
+            PrimeCalc calc = new();
+            _ = calc.IsPrime(9049); // force creation of list above 97 * 97
+
+            Assert.Equal(result, calc.PrimeListToViewDesc(firstNrOfPrimes));
+
+        }
+
 
         // result 2 | 3 | 5 | 7 | 11 | 13 | 17 | 19 | 23 | 29 | 31 | 37 | 41 | 43 | 47 | 53 | 59 | 61 | 67 | 71 | ... (46 primes)
         [Theory]
@@ -58,15 +76,111 @@ namespace XUnitTestCyMath
         [InlineData(5081, true)]
         [InlineData(5087, true)]
         [InlineData(5099, true)]
-        [InlineData(10000397, true)]
+        [InlineData(10086647, false)]
+        [InlineData(841, false)]
+        [InlineData(10000379, true)]
+        [InlineData(10000397, false)]
         [InlineData(100000393, true)]
         [InlineData(1000000403, true)]
         [InlineData(1000000407, false)]
         [InlineData(600851475143, false)]
+        [InlineData(600851475583, true)]
+        [InlineData(18848997161, true)]
+        [InlineData(18848997157, false)]
+        [InlineData(188489971511, false)]
         public void PrimeCalc_IsPrime(long value, bool result)
         {
             PrimeCalc calc = new();
             Assert.Equal(result, calc.IsPrime(value));
+        }
+
+
+
+        [Fact]
+        public void PrimCalc_Factors()
+        {
+            long testNr = 2 * 2 * 3 * 5 * 5; // 300
+            PrimeCalc calc = new();
+
+            var list = calc.GetPrimeFactors(testNr);
+
+            Assert.Equal(3, list.Count);
+            Assert.Equal(2, list[0].Prime);
+            Assert.Equal(2, list[0].Count);
+
+            Assert.Equal(3, list[1].Prime);
+            Assert.Equal(1, list[1].Count);
+
+            Assert.Equal(5, list[2].Prime);
+            Assert.Equal(2, list[2].Count);
+
+            //// big nr:
+            testNr = 600851475143;
+            list = calc.GetPrimeFactors(testNr);
+            //Assert.Equal(4, list.Count);
+
+            //Assert.Equal(71, list[0].Prime);
+            //Assert.Equal(1, list[0].Count);
+
+            //Assert.Equal(839, list[1].Prime);
+            //Assert.Equal(1, list[1].Count);
+
+            //Assert.Equal(1471, list[2].Prime);
+            //Assert.Equal(1, list[2].Count);
+
+            //Assert.Equal(6857, list[3].Prime);
+            //Assert.Equal(1, list[3].Count);
+
+            // big nr:
+            testNr = 188489971511;
+             list = calc.GetPrimeFactors(testNr);
+            Assert.Equal(2, list.Count);
+
+            Assert.Equal(17, list[0].Prime);
+            Assert.Equal(1, list[0].Count);
+
+            Assert.Equal(11087645383, list[1].Prime);
+            Assert.Equal(1, list[1].Count);
+
+
+        }
+
+
+
+        [Fact]
+        public void PrimCalc_FactorsMulti()
+        {
+            long testNr = 600851475;
+            PrimeCalc calc = new();
+
+            var list = calc.GetPrimeFactors(testNr);
+
+            Assert.Equal(4, list.Count);
+
+            Assert.Equal(3, list[0].Prime);
+            Assert.Equal(2, list[0].Count);
+
+            Assert.Equal(5, list[1].Prime);
+            Assert.Equal(2, list[1].Count);
+
+            Assert.Equal(7, list[2].Prime);
+            Assert.Equal(2, list[2].Count);
+
+            Assert.Equal(54499, list[3].Prime);
+            Assert.Equal(1, list[3].Count);
+
+
+            testNr = 18848997157;
+            var list2 = calc.GetPrimeFactors(testNr);
+
+            Assert.Equal(2, list2.Count);
+
+            Assert.Equal(13729, list2[0].Prime);
+            Assert.Equal(1, list2[0].Count);
+
+            Assert.Equal(1372933, list2[1].Prime);
+            Assert.Equal(1, list2[1].Count);
+
         }
 
     }
